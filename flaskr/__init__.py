6fff2539103b0be138b47c06230da16eb8e19a74
DLo930 <dlo2@andrew.cmu.edu>
2019-09-07 15:34:44 -0400

    Add socket response to connection

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,21 +2,35 @@ import os
 
 import firebase_admin
 from firebase_admin import credentials
-from flask import Flask, render_template
+from firebase_admin import firestore
+from flask import Flask
 from flask_socketio import SocketIO
-
-
-cred = credentials.Certificate('path/to/serviceAccountKey.json')
-firebase_admin.initialize_app(cred)
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
-@socketio.on('message')
-def handleMessage(message):
-    send(message, broadcast=True)
+cred = credentials.Certificate('stork-20b75-firebase-adminsdk-ml5n9-9eac6fb09d.json')
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@socketio.on('connect')
+def test_connect():
+    emit('my response', {'data': 'Connected'})
+
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Client disconnected')
+
+@socketio.on('list post')
+def handle_list_post(json):
+    print(f'Received json: {str(json)}')
+    emit('list posted', json, broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(app)
